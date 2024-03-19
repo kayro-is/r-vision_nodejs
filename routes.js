ba31cd7e5470e4efb82d6c2722eee71c65ebdc07
kayro-is <luffykosuki@gmail.com>
2024-03-20 00:25:57 +0100

    définir les route pour les opération Crud sur les article

---
+++ routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// Importation des controlleurs pour les articles
+const articlesController = require('./controllers/articlesController.js');
+
+// création d'un router express
+const router = express.Router();
+
+// Déffinition des routes pour les opération Crud sur les articles
+router.post('/articles', articlesController.createArticle); // Crée un article
+router.get('/articles', articlesController.getAllArticles); // lire tous les articles 
+router.get('/articles', articlesController.getAllArticleById); // lire un article par son ID
+router.put('/articles', articlesController.updateArticle); // mettre a jour un article par son ID
+router.delete('/articles', articlesController.deleteArticle); //supprimer un article par son ID
+
+
+module.exports = router;
